0b7865c2901a2eb728b39fd6f3b9febff82b2e26
Muhammad-Hamza-Khan-03 <hamzakhan102003@gmail.com>
2025-06-06 14:50:12 +0500

    deploying

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ["https://deploy-mern-1whq.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
